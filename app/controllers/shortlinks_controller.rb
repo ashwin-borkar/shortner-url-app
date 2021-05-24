f34bf9f6d1beb160030404b63cf6e407e6b49b9e
ryan erswell <ersie93@googlemail.com>
2021-05-24 13:07:13 +0100

    added logout and timeout

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -6,14 +6,20 @@ class ShortlinksController < ApplicationController
 
 	def create
 		client = Bitly::API::Client.new(token: ENV['BITLY_API'])
-		bitlink = client.shorten(long_url: params[:shortlink][:original])
-		@shortlink = Shortlink.new(original: params[:shortlink][:original], url: bitlink.link)
+		bitlink = client.shorten(long_url: shortlink_params[:original])
+		@shortlink = Shortlink.new(original: shortlink_params[:original], url: bitlink.link)
 		@shortlink.user = current_user
 		if @shortlink.save
 			redirect_to root_path
 		else
 			redirect_to root_path 
 		end
+	end
+
+	private
+
+	def shortlink_params
+		params.require(:shortlink).permit(:original)
 	end
 
 end
